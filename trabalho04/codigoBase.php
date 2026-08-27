264382db53b32d4133c07ea97c8a1d17be361c2f
gustavoramosmarcondes <gustavo.ramos.marcondes@gmail.com>
2026-08-26 21:31:15 -0300

    a

---
+++ trabalho04/codigoBase.php
@@ -0,0 +1,10 @@
+<?php
+
+$num1 = (float) readline("DIgite o primiero número: ");
+$num2 = (float) readline("DIgite o segundo número: ");
+
+$soma = $num1 + $num2;
+
+echo "A soma entre $num1 e $num2 é: $soma\n";
+
+?>
